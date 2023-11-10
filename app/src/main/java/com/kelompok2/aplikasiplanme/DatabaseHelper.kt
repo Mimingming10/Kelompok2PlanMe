eb9fe1beac2f7c977be9bc11161333de34798c2d
icachintyas <aicachintya@gmail.com>
2023-11-10 14:29:44 +0700

    Menambahkan class databaseHelper

--- app/src/main/java/com/kelompok2/aplikasiplanme/DatabaseHelper.kt
+++ app/src/main/java/com/kelompok2/aplikasiplanme/DatabaseHelper.kt
@@ -13,4 +13,23 @@ class DatabaseHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME,
             "$COL_USER_NAME TEXT," +
             "$COL_USER_EMAIL TEXT," +
             "$COL_USER_PASSWORD TEXT)"
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CREATE_TABLE_USER)
+    }
+    private val DROP_TABLE_USER = "DROP TABLE IF EXISTS $TABLE_USER"
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL(DROP_TABLE_USER)
+    }
+
+    fun registerUser() {}
+    companion object{
+        private const val DATABASE_VERSION =1
+        private const val DATABASE_NAME = "user.db"
+        private const val TABLE_USER = "tbl_user"
+
+        private const val COL_USER_ID ="user_id"
+        private const val COL_USER_NAME ="user_name"
+        private const val COL_USER_EMAIL ="user_email"
+        private const val COL_USER_PASSWORD ="user_password"
+    }
 }
