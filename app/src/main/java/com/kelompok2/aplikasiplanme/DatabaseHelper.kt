667dcab7c0efd2f8c8777af4e93c989d42a504a6
icachintyas <aicachintya@gmail.com>
2023-11-10 16:41:53 +0700

    menambahkan class user dan validationUser serta menyambungkan peda clas login dan registrasi

--- app/src/main/java/com/kelompok2/aplikasiplanme/DatabaseHelper.kt
+++ app/src/main/java/com/kelompok2/aplikasiplanme/DatabaseHelper.kt
@@ -1,8 +1,10 @@
 package com.kelompok2.aplikasiplanme
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.os.FileObserver.CREATE
+import android.provider.ContactsContract.CommonDataKinds.Email
 
 class DatabaseHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME,null,
     DATABASE_VERSION) {
@@ -21,15 +23,48 @@ class DatabaseHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME,
         db?.execSQL(DROP_TABLE_USER)
     }
 
-    fun registerUser() {}
-    companion object{
-        private const val DATABASE_VERSION =1
-        private const val DATABASE_NAME = "user.db"
-        private const val TABLE_USER = "tbl_user"
+    fun registerUser(user:User){
+        val db = this.writableDatabase
+        val value = ContentValues()
+        value.put(COL_USER_NAME,user.username)
+        value.put(COL_USER_EMAIL,user.email)
+        value.put(COL_USER_PASSWORD,user.password)
+        db.insert(TABLE_USER,null, value)
+        db.close()
+    }
+    fun loginUser(email: String, password: String):Boolean{
+      val columns = arrayOf(COL_USER_ID)
+      val db = this.readableDatabase
+        val selection = "$COL_USER_EMAIL = ? AND $COL_USER_PASSWORD = ?"
+        val selectionArgs = arrayOf(email, password)
 
-        private const val COL_USER_ID ="user_id"
-        private const val COL_USER_NAME ="user_name"
-        private const val COL_USER_EMAIL ="user_email"
-        private const val COL_USER_PASSWORD ="user_password"
+      val cursor = db.query(
+            TABLE_USER,
+          columns,
+          selection,
+          selectionArgs,
+           null,
+        null,
+         null
+        )
+         val cursorCount = cursor.count
+         cursor.close()
+         db.close()
+
+         return cursorCount > 0
+
+    }
+    companion object{
+            private const val DATABASE_VERSION =1
+            private const val DATABASE_NAME = "user.db"
+            private const val TABLE_USER = "tbl_user"
+
+            private const val COL_USER_ID ="user_id"
+            private const val COL_USER_NAME ="user_name"
+            private const val COL_USER_EMAIL ="user_email"
+            private const val COL_USER_PASSWORD ="user_password"
     }
 }
+
+
+
--- app/src/main/java/com/kelompok2/aplikasiplanme/LoginActivity.kt
+++ app/src/main/java/com/kelompok2/aplikasiplanme/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.kelompok2.aplikasiplanme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.kelompok2.aplikasiplanme.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -17,8 +18,25 @@ class LoginActivity : AppCompatActivity() {
         binding.klikdisini.setOnClickListener{
             startActivity(Intent(this,RegisterActivity::class.java))
         }
+        binding.button.setOnClickListener {
+            loginUser()
+        }
     }
 
+    private fun loginUser() {
+        val email = binding.textinputUsername.text.toString().trim()
+        val password = binding.textinputPassword.text.toString().trim()
+
+        if(ValidationUtils.isTextNotEmpty(email) && ValidationUtils.isTextNotEmpty(password)){
+           if(ValidationUtils.isValidEmail(email)) {
+
+           }else{
+               Toast.makeText(this, "Email tidak valid", Toast.LENGTH_SHORT).show()
+           }
+        }else{
+            Toast.makeText(this, "Tolong inputkan semua", Toast.LENGTH_SHORT).show()
+        }
+    }
 
 
 }
