ce1e62285482887a193a0a5e17187625433ee29f
Mimingming10 <natasyain10@gmail.com>
2023-11-11 10:03:34 +0700

    perbaikkan

--- app/src/main/java/com/kelompok2/aplikasiplanme/LoginActivity.kt
+++ app/src/main/java/com/kelompok2/aplikasiplanme/LoginActivity.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.kelompok2.aplikasiplanme.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,23 +17,24 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.klikdisini.setOnClickListener{
-            startActivity(Intent(this,RegisterActivity::class.java))
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
         binding.button.setOnClickListener {
             loginUser()
         }
     }
 
+
     private fun loginUser() {
         val email = binding.textinputUsername.text.toString().trim()
         val password = binding.textinputPassword.text.toString().trim()
 
         if(ValidationUtils.isTextNotEmpty(email) && ValidationUtils.isTextNotEmpty(password)){
-           if(ValidationUtils.isValidEmail(email)) {
+            if(ValidationUtils.isValidEmail(email)) {
 
-           }else{
-               Toast.makeText(this, "Email tidak valid", Toast.LENGTH_SHORT).show()
-           }
+            }else{
+                Toast.makeText(this, "Email tidak valid", Toast.LENGTH_SHORT).show()
+            }
         }else{
             Toast.makeText(this, "Tolong inputkan semua", Toast.LENGTH_SHORT).show()
         }
