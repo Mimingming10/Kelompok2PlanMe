ab2ec0edc6e63c1441a40b6e74b2786b16d3cfe4
Mimingming10 <natasyain10@gmail.com>
2023-12-13 19:01:41 +0700

    First Commit

---
+++ app/src/main/java/com/kelompok2/aplikasiplanme/KuliahMainActivity.kt
@@ -0,0 +1,11 @@
+package com.kelompok2.aplikasiplanme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KuliahMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kuliah_main)
+    }
+}
---
+++ app/src/main/java/com/kelompok2/aplikasiplanme/Users.kt
@@ -0,0 +1,10 @@
+package com.kelompok2.aplikasiplanme
+
+data class Kerja(
+    val kerjaId : String? = null,
+    val kerjaName : String? = null,
+    val kerjaEmail : String? = null,
+    val kerjaPassword : String? = null,
+    val kerjaImage : String? = null
+)
+
