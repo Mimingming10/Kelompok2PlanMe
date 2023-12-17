83585b788a2b74a09cb5565a02b4edef06aa36d3
Mimingming10 <natasyain10@gmail.com>
2023-12-17 23:01:34 +0700

    perbaruan

--- app/src/main/java/com/kelompok2/aplikasiplanme/HomeActivity.kt
+++ app/src/main/java/com/kelompok2/aplikasiplanme/HomeActivity.kt
@@ -11,9 +11,8 @@ import com.kelompok2.aplikasiplanme.databinding.ActivityHomeBinding
 
 
 class HomeActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var addButton: FloatingActionButton
-    private var isOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,34 +22,10 @@ class HomeActivity : AppCompatActivity() {
         // Menghapus title project pada bagian atas
         supportActionBar?.hide()
 
-
-        addButton = findViewById(R.id.tambah)
-
-        addButton.setOnClickListener {
-            toggleFabAnimation()
-        }
         binding.tambah.setOnClickListener {
             val intent = Intent(this, CreateNoteActivity::class.java)
             startActivity(intent)
         }
-    }
 
-
-    private fun toggleFabAnimation() {
-        val rotateOpen = AnimationUtils.loadAnimation(this, R.anim.fab_rotate)
-        val rotateClose = AnimationUtils.loadAnimation(this, R.anim.fab_rotate_anticlock)
-        val fabOpen = AnimationUtils.loadAnimation(this, R.anim.fab_open)
-        val fabClose = AnimationUtils.loadAnimation(this, R.anim.fab_close)
-
-        if (isOpen) {
-            addButton.startAnimation(rotateClose)
-            // Tambahkan logika lain yang diinginkan saat tombol ditutup
-        } else {
-            addButton.startAnimation(rotateOpen)
-            // Tambahkan logika lain yang diinginkan saat tombol dibuka
-        }
-
-        isOpen = !isOpen
-    }
 }
 
